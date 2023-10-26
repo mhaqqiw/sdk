630398ba08a79c4d502aee3efef092a39e055d4e
haqqi.wicaksono <haqqi.wicaksono@tokopedia.com>
2023-10-26 10:28:34 +0700

    improve trace

--- go/utils/qlog/logging.go
+++ go/utils/qlog/logging.go
@@ -3,6 +3,7 @@ package qlog
 import (
 	"fmt"
 	"log"
+	"os"
 	"runtime"
 	"strings"
 
@@ -10,17 +11,48 @@ import (
 	"github.com/mhaqqiw/sdk/go/qentity"
 )
 
-func Trace() string {
-	pc := make([]uintptr, 10) // at least 1 entry needed
-	runtime.Callers(2, pc)
-	f := runtime.FuncForPC(pc[0])
-	_, line := f.FileLine(pc[0])
-	s := f.Name()
-	index := strings.LastIndex(s, ".")
-	if index > 0 {
-		s = s[:index]
+func getRelativePath(absolutePath string) string {
+	// Get the current working directory
+	currentDir, err := os.Getwd()
+	if err != nil {
+		// Handle the error, e.g., log it or use a default path
+		return absolutePath
 	}
-	return fmt.Sprintf("%s:%d - %s", s, line, f.Name())
+
+	// Check if the absolute path starts with the current working directory
+	if strings.HasPrefix(absolutePath, currentDir) {
+		// Get the relative path by removing the current working directory from the beginning
+		relativePath := absolutePath[len(currentDir):]
+
+		// Remove the leading slash if present
+		if strings.HasPrefix(relativePath, "/") {
+			relativePath = relativePath[1:]
+		}
+
+		return relativePath
+	}
+
+	// If the absolute path does not start with the current working directory, return the original path
+	return absolutePath
+}
+
+func Trace() string {
+	pc := make([]uintptr, 15)
+	runtime.Callers(2, pc)
+	frames := runtime.CallersFrames(pc)
+
+	for {
+		frame, more := frames.Next()
+		if !more {
+			break
+		}
+		if !strings.Contains(frame.File, "runtime/") {
+			relativePath := getRelativePath(frame.File)
+			return fmt.Sprintf("%s:%d - %s", relativePath, frame.Line, frame.Function)
+		}
+	}
+
+	return "unable to get trace"
 }
 
 func LogPrint(typeLog string, identifier string, trace string, err string, monitoring qentity.Monitoring) {
