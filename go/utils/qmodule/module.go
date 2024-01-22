9736aac9b9ef4da3d8e130065457669a0ba8abd7
haqqi.wicaksono <haqqi.wicaksono@tokopedia.com>
2024-01-22 10:08:12 +0700

    update to support UUID v7

--- go/utils/qmodule/module.go
+++ go/utils/qmodule/module.go
@@ -5,22 +5,57 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/mhaqqiw/sdk/go/qentity"
 )
 
 func CountElapsed(start time.Time) time.Duration {
 	return time.Since(start)
 }
 
-func GetDataFromUUIDV1(uuid uuid.UUID) (time.Time, int, string) {
-	t := uuid.Time()
+func GetDataFromUUID(uuidStr string) (qentity.UUIDData, error) {
+	nodeID := "00:00:00:00:00:00"
+	res := qentity.UUIDData{}
+	u, err := uuid.Parse(uuidStr)
+	if err != nil {
+		return res, err
+	}
+
+	t := u.Time()
 	sec, nsec := t.UnixTime()
 	timeStamp := time.Unix(sec, nsec)
 
-	// Extract the clock sequence (14 bits)
-	clockSequence := uuid.ClockSequence()
+	byteData, err := u.MarshalText()
+	if err != nil {
+		return res, err
+	}
 
-	// Extract the node (48 bits)
-	node := fmt.Sprintf("%02X:%02X:%02X:%02X:%02X:%02X", uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15])
+	val, err := u.Value()
+	if err != nil {
+		return res, err
+	}
 
-	return timeStamp, clockSequence, node
+	n := u.NodeID()
+
+	if len(n) == 6 {
+		nodeID = fmt.Sprintf("%02X:%02X:%02X:%02X:%02X:%02X", n[0], n[1], n[2], n[3], n[4], n[5])
+	}
+
+	res.UUID = u
+	res.Timestamp = timeStamp
+	res.ClockSequence = u.ClockSequence()
+	res.Domain = u.Domain().String()
+	res.NodeID = nodeID
+	res.ID = u.ID()
+	res.MarshalText = string(byteData)
+	res.String = u.String()
+	res.Version = u.Version().String()
+	res.URN = u.URN()
+	res.Value = val.(string)
+	res.Variant = u.Variant().String()
+
+	if res.Version != "VERSION_1" {
+		res.NodeID = "00:00:00:00:00:00"
+		nodeID = "00:00:00:00:00:00"
+	}
+	return res, nil
 }
