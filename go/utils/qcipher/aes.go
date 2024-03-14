6644801470a1d7d272d742d7084a0cbe06201254
Haqqi <haqqi.wicaksono@tokopedia.com>
2024-03-14 08:22:00 +0700

    add cipher module

---
+++ go/utils/qcipher/aes.go
@@ -0,0 +1,69 @@
+package qcipher
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func AESEncrypt(txt string, key string) (string, error) {
+	keyByte, err := hex.DecodeString(key)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(keyByte)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len([]byte(txt)))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(txt))
+
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+func AESDecrypt(ciphertext string, key string) ([]byte, error) {
+	keyByte, err := hex.DecodeString(key)
+	if err != nil {
+		return nil, err
+	}
+	data, err := base64.URLEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(keyByte)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < aes.BlockSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	iv := data[:aes.BlockSize]
+	data = data[aes.BlockSize:]
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(data, data)
+
+	return data, nil
+}
+
+func GenAESKey() (string, error) {
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(key), nil
+}
