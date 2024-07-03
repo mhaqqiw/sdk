cbd935be193cc44852e257cc4a9dc767ea81025c
haqqi <q2525252525@gmail.com>
2024-07-03 12:56:00 +0700

    update rawMessage

--- go/utils/qhttp/request.go
+++ go/utils/qhttp/request.go
@@ -1,6 +1,7 @@
 package qhttp
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"time"
@@ -23,16 +24,30 @@ func Return(c *gin.Context, statusCode int, message interface{}) {
 	a := time.Time{}
 	mapstructure.Decode(start, &a)
 
+	var rawMessage json.RawMessage
+	var err error
+	switch v := message.(type) {
+	case string:
+		rawMessage = json.RawMessage(v)
+	case []byte:
+		rawMessage = json.RawMessage(v)
+	default:
+		rawMessage, err = json.Marshal(v) // Best-effort conversion to JSON
+		if err != nil {
+			qlog.LogPrint(qconstant.ERROR, "json.Marshal", qlog.Trace(), err.Error())
+		}
+	}
+
 	if statusCode >= 1 && statusCode < 300 {
-		var res = qentity.Response{Status: "ok", Code: statusCode, Message: message, ProcessTime: qmodule.CountElapsed(a), Version: Metadata.Version}
+		var res = qentity.Response{Status: "ok", Code: statusCode, Message: rawMessage, ProcessTime: qmodule.CountElapsed(a), Version: Metadata.Version}
 		c.IndentedJSON(int(res.Code), res)
 	} else if statusCode >= 300 && statusCode < 400 {
 		c.Redirect(statusCode, fmt.Sprintf("%v", message))
 	} else if statusCode >= 400 && statusCode < 600 {
-		var res = qentity.Response{Status: "error", Code: statusCode, Message: message, ProcessTime: qmodule.CountElapsed(a), Version: Metadata.Version}
+		var res = qentity.Response{Status: "error", Code: statusCode, Message: rawMessage, ProcessTime: qmodule.CountElapsed(a), Version: Metadata.Version}
 		c.AbortWithStatusJSON(statusCode, res)
 	} else {
-		var res = qentity.Response{Status: "error", Code: 500, Message: fmt.Sprintf("http status code: %v is not listed", statusCode), ProcessTime: qmodule.CountElapsed(a), Version: Metadata.Version}
+		var res = qentity.Response{Status: "error", Code: 500, Message: json.RawMessage(fmt.Sprintf(`{"error":"http status code: %v is not listed"}`, statusCode)), ProcessTime: qmodule.CountElapsed(a), Version: Metadata.Version}
 		c.AbortWithStatusJSON(statusCode, res)
 	}
 }
